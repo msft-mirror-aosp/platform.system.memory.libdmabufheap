5d15236276ca7bb98417bd2f8ccaf0cc67a021f6
Krzysztof Kosiński <krzysio@google.com>
2025-07-04 03:44:41 +0000

    Fix -Wstrict-prototypes.
    In C, functions without parameters need to explicitly specify
    the parameter list as "foo(void)". Unlike in C++, "foo()" in C
    is an old deprecated syntax for "forward-declaring" a function
    without a prototype.
    
    This is the only file in Android that triggers this warning.
    
    Bug: 296606895
    Test: presubmit
    Flag: EXEMPT trivial warning fix
    Change-Id: Ia3b62708d3e3b95c342469745c96540a79e4f466

--- include/BufferAllocator/BufferAllocatorWrapper.h
+++ include/BufferAllocator/BufferAllocatorWrapper.h
@@ -28,7 +28,7 @@ typedef class BufferAllocator BufferAllocator;
 typedef struct BufferAllocator BufferAllocator;
 #endif
 
-BufferAllocator* CreateDmabufHeapBufferAllocator();
+BufferAllocator* CreateDmabufHeapBufferAllocator(void);
 
 void FreeDmabufHeapBufferAllocator(BufferAllocator* buffer_allocator);
 
@@ -52,7 +52,7 @@ int DmabufHeapCpuSyncEnd(BufferAllocator* buffer_allocator, unsigned int dmabuf_
                          SyncType sync_type, int (*legacy_ion_cpu_sync)(int, int, void*),
                          void* legacy_ion_custom_data);
 
-bool CheckIonSupport();
+bool CheckIonSupport(void);
 
 #ifdef __cplusplus
 }
