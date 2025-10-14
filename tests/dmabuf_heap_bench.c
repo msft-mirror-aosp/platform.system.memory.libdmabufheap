35806e49de24a1aaf5ba1f33263f9cd4c09c63f3
Priyanka Advani (xWF) <padvani@google.com>
2025-10-14 15:20:12 -0700

    Revert "libdmabufheap: Remove most ION support"
    This reverts commit 86cb5c99326b3ec1145687c2279e6c23b2ffb822.
    
    Reason for revert: Droidmonitor created revert due to b/452122466,b/452121154. Will be verifying through ABTD before submission.
    
    Fix: 452121154
    Fix: 452122466
    Bug: 444679893
    Change-Id: Ia84eb7b541efafe330b2f424e188d63ba92720a2

--- tests/dmabuf_heap_bench.c
+++ tests/dmabuf_heap_bench.c
@@ -27,10 +27,14 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <drm/drm.h>
 #include <linux/dma-buf.h>
 #include <linux/dma-heap.h>
+#include <linux/ion.h>
+#include <linux/ion_4.19.h>
 
 #define HEAP_DEVPATH "/dev/dma_heap"
+#define ION_DEVPATH "/dev/ion"
 
 #define ONE_MEG (1024 * 1024)
 #define NUM_SIZES 4
@@ -38,6 +42,39 @@ int sizes[NUM_SIZES] = {4 * 1024, ONE_MEG, 8 * ONE_MEG, 32 * ONE_MEG};
 
 #define NUM_ITERS 5000
 #define NSEC_PER_SEC 1000000000LL
+#define MAX_HEAP_COUNT ION_HEAP_TYPE_CUSTOM
+
+int ion_heap_open(void) {
+    int ret, fd;
+    char buf[256];
+
+    ret = sprintf(buf, "%s", ION_DEVPATH);
+    if (ret < 0) {
+        printf("sprintf failed!\n");
+        return ret;
+    }
+
+    fd = open(buf, O_RDWR);
+    if (fd < 0) printf("open %s failed!\n", buf);
+    return fd;
+}
+
+int ion_heap_alloc(int ionfd, int heap_id, size_t len, unsigned int flags, int* dmabuf_fd) {
+    struct ion_new_allocation_data alloc_data;
+    int ret;
+
+    memset(&alloc_data, 0, sizeof(alloc_data));
+    alloc_data.heap_id_mask = 1 << heap_id;
+    alloc_data.flags = flags;
+    alloc_data.len = len;
+
+    /* Allocate memory for this ION client as per heap_type */
+    ret = ioctl(ionfd, ION_IOC_NEW_ALLOC, &alloc_data);
+
+    *dmabuf_fd = alloc_data.fd;
+
+    return ret;
+}
 
 int dmabuf_heap_open(char* name) {
     int ret, fd;
@@ -79,6 +116,57 @@ void dmabuf_sync(int fd, int start_stop) {
     if (ret) printf("sync failed %d\n", errno);
 }
 
+void ion_heap_bench(unsigned int heap_type, int size, int flags) {
+    int heap_id;
+    int ionfd = -1, dmabuf_fd = -1;
+    struct ion_heap_query query;
+    struct ion_heap_data heap_data[MAX_HEAP_COUNT];
+    struct timespec ts_start, ts_end;
+    long long start, end;
+    int ret;
+    unsigned int i;
+
+    ionfd = ion_heap_open();
+    if (ionfd < 0) return;
+
+    memset(&query, 0, sizeof(query));
+    query.cnt = MAX_HEAP_COUNT;
+    query.heaps = (unsigned long int)&heap_data[0];
+    /* Query ION heap_id_mask from ION heap */
+    ret = ioctl(ionfd, ION_IOC_HEAP_QUERY, &query);
+    if (ret < 0) {
+        printf("<%s>: Failed: ION_IOC_HEAP_QUERY: %s\n", __func__, strerror(errno));
+        goto out;
+    }
+    heap_id = MAX_HEAP_COUNT + 1;
+    for (i = 0; i < query.cnt; i++) {
+        if (heap_data[i].type == heap_type) {
+            heap_id = heap_data[i].heap_id;
+            break;
+        }
+    }
+    if (heap_id > MAX_HEAP_COUNT) {
+        printf("<%s>: ERROR: heap type does not exists\n", __func__);
+        goto out;
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &ts_start);
+    for (i = 0; i < NUM_ITERS; i++) {
+        ret = ion_heap_alloc(ionfd, heap_id, size, flags, &dmabuf_fd);
+        if (ret) goto out;
+        close(dmabuf_fd);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &ts_end);
+
+    start = ts_start.tv_sec * NSEC_PER_SEC + ts_start.tv_nsec;
+    end = ts_end.tv_sec * NSEC_PER_SEC + ts_end.tv_nsec;
+
+    printf("ion heap:    alloc %d bytes %i times in %lld ns \t %lld ns/call\n", size, NUM_ITERS,
+           end - start, (end - start) / NUM_ITERS);
+out:
+    if (ionfd >= 0) close(ionfd);
+}
+
 void dmabuf_heap_bench(char* heap_name, int size) {
     int heap_fd = -1, dmabuf_fd = -1;
     struct timespec ts_start, ts_end;
@@ -108,18 +196,30 @@ out:
 
 int main(int argc, char* argv[]) {
     char* dmabuf_heap_name;
+    unsigned int ion_heap_type;
+    int ion_flags = 0;
+    int testing_ion = 0;
     int i;
     if (argc < 2) {
-        printf("Usage %s <dmabuf heap name>\n", argv[0]);
+        printf("Usage %s [-i <ion heap type> <ion heap flags>] <dmabuf heap name>\n", argv[0]);
         return -1;
     }
 
-    dmabuf_heap_name = argv[1];
+    if (argv[1][0] == '-' && argv[1][1] == 'i') {
+        testing_ion = 1;
+        ion_heap_type = strtol(argv[2], NULL, 0);
+        ion_flags = strtol(argv[3], NULL, 0);
+        dmabuf_heap_name = argv[4];
+    } else {
+        dmabuf_heap_name = argv[1];
+    }
 
     printf("Testing dmabuf %s", dmabuf_heap_name);
+    if (testing_ion) printf(" vs ion heaptype %d (flags: 0x%x)", ion_heap_type, ion_flags);
     printf("\n---------------------------------------------\n");
     for (i = 0; i < NUM_SIZES; i++) {
         dmabuf_heap_bench(dmabuf_heap_name, sizes[i]);
+        if (testing_ion) ion_heap_bench(ion_heap_type, sizes[i], ion_flags);
     }
 
     return 0;
--- tests/dmabuf_heap_test.c
+++ tests/dmabuf_heap_test.c
@@ -15,13 +15,41 @@
  */
 
 #include <BufferAllocator/BufferAllocatorWrapper.h>
+#include <errno.h>
+#include <ion/ion.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-void libdmabufheaptest() {
+typedef struct {
+    int a;
+    int b;
+} custom_callback_data;
+
+int legacy_ion_custom_callback(int ion_fd, int dma_buf_fd,
+                                void *legacy_ion_custom_data) {
+    int ret = 0;
+    custom_callback_data *data;
+    (void) dma_buf_fd;
+    if (!ion_is_legacy(ion_fd)) {
+        perror("error in legacy ion custom callback");
+        ret = errno;
+    } else {
+        printf("in custom legacy ion cpu sync callback\n");
+        if (legacy_ion_custom_data) {
+            data = (custom_callback_data *) legacy_ion_custom_data;
+            printf("custom data was supplied to ion cpu sync callback: a = %d, b = %d\n",
+                   data->a, data->b);
+        }
+    }
+
+    return ret;
+}
+
+void libdmabufheaptest(bool use_custom_callback, void *legacy_ion_custom_data) {
     const size_t len = 1024 * 1024;
     int fd = -1, ret = 0;
     size_t i = 0;
@@ -30,6 +58,21 @@ void libdmabufheaptest() {
     BufferAllocator* bufferAllocator = CreateDmabufHeapBufferAllocator();
     if (!bufferAllocator) {
         printf("unable to get allocator\n");
+        return;
+    }
+
+    /*
+     * Legacy ion devices may have hardcoded heap IDs that do not
+     * match the ion UAPI header. Map heap name 'system' to a heap mask
+     * of all 1s so that these devices will allocate from the first
+     * available heap when asked to allocate from a heap of name 'system'.
+     */
+    ret = MapDmabufHeapNameToIonHeap(bufferAllocator, kDmabufSystemHeapName,
+                                     "" /* no mapping for non-legacy */,
+                                     0 /* no mapping for non-legacy ion */,
+                                     ~0 /* legacy ion heap mask */, 0 /* legacy ion heap flag */);
+    if (ret < 0) {
+        printf("MapDmabufHeapNameToIonHeap failed: %d\n", ret);
         return;
     }
 
@@ -62,7 +105,9 @@ void libdmabufheaptest() {
         return;
     }
 
-    ret = DmabufHeapCpuSyncStart2(bufferAllocator, fd, kSyncReadWrite);
+    ret = DmabufHeapCpuSyncStart(bufferAllocator, fd, kSyncReadWrite,
+                                 use_custom_callback ? legacy_ion_custom_callback : NULL,
+                                 legacy_ion_custom_data);
     if (ret) {
         printf("DmabufHeapCpuSyncStart failed: %d\n", ret);
         return;
@@ -80,7 +125,9 @@ void libdmabufheaptest() {
         }
     }
 
-    ret = DmabufHeapCpuSyncEnd2(bufferAllocator, fd, kSyncReadWrite);
+    ret = DmabufHeapCpuSyncEnd(bufferAllocator, fd, kSyncReadWrite,
+                               use_custom_callback ? legacy_ion_custom_callback : NULL,
+                               legacy_ion_custom_data);
     if (ret) {
         printf("DmabufHeapCpuSyncEnd failed: %d\n", ret);
         return;
@@ -93,7 +140,23 @@ void libdmabufheaptest() {
     printf("PASSED\n");
 }
 
-int main(int, char*[]) {
-    libdmabufheaptest();
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+
+    if (CheckIonSupport())
+        printf("ION support detected\n");
+    else
+        printf("No ION support detected\n");
+
+    custom_callback_data data = {.a = 1, .b = 2};
+    printf("*****running with custom legacy ion cpu sync callback, with custom data****\n");
+    libdmabufheaptest(true, &data);
+    printf("*****running with custom legacy ion cpu sync callback, without custom data****\n");
+    libdmabufheaptest(true, NULL);
+    printf("****running without custom legacy ion cpu sync callback, with custom data****\n");
+    libdmabufheaptest(false, &data);
+    printf("****running without custom legacy ion cpu sync callback, without custom data****\n");
+    libdmabufheaptest(false, NULL);
     return 0;
 }
